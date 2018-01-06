f28e0d644559a48da37249854687c65c846bb748
julia- <julia.ryanx@gmail.com>
2018-01-07 01:03:41 +1100

    change devServer for client-side routing

--- webpack.config.js
+++ webpack.config.js
@@ -19,16 +19,13 @@ module.exports = {
       },
       {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
   devtool: 'cheap-module-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
   }
 }
