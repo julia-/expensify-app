66cdfe2b3fb22d4a930579ba18c474c7876996b1
julia- <julia.ryanx@gmail.com>
2018-01-07 00:36:54 +1100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+// console.log(__dirname) // gets the absolute path
+
+module.exports = {
+  // tells webpack where it should start
+  entry: './src/app.js',
+  output: {
+    // need to provide an absolute path
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+}
