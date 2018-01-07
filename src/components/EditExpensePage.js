154ae8abdee7bcadcf9050e3a0350e9d0fc2ca26
julia- <julia.ryanx@gmail.com>
2018-01-07 14:20:07 +1100

    created component to edit expense

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditExpensePage = props => {
+  return (
+    <div>
+      <p>EditExpensePage for {props.match.params.id}</p>
+    </div>
+  )
+}
+
+export default EditExpensePage
