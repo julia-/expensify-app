57c9b7303e90cfccab29fa6a90e45f219e57e719
julia- <julia.ryanx@gmail.com>
2018-01-07 14:22:27 +1100

    import AppRouter and remove components and routes to own files

--- src/app.js
+++ src/app.js
@@ -1,50 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'normalize.css/normalize.css'
+
+import AppRouter from './routers/AppRouter'
 
 import './styles/styles.scss'
 
-const ExpenseDashboardPage = () => (
-  <div>
-    <p>ExpenseDashboardPage</p>
-  </div>
-)
-
-const AddExpensePage = () => (
-  <div>
-    <p>AddExpensePage</p>
-  </div>
-)
-
-const EditExpensePage = () => (
-  <div>
-    <p>EditExpensePage</p>
-  </div>
-)
-
-const HelpPage = () => (
-  <div>
-    <p>HelpPage</p>
-  </div>
-)
-
-const NotFoundPage = () => (
-  <div>
-    <p>404</p>
-  </div>
-)
-
-const routes = (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} exact={true} />
-      <Route path="/edit" component={EditExpensePage} exact={true} />
-      <Route path="/help" component={HelpPage} exact={true} />
-      <Route component={NotFoundPage} />
-    </Switch>
-  </BrowserRouter>
-)
-
-ReactDOM.render(routes, document.getElementById('app'))
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
