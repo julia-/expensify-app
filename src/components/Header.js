9f420e1c397a93218d6c9a159d54d85c6a483e7d
julia- <julia.ryanx@gmail.com>
2018-01-07 14:20:53 +1100

    create Header component with NavLinks

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => (
+  <header>
+    <h1>Expensify App</h1>
+    <NavLink exact to="/" activeClassName="is-active">
+      Dashboard
+    </NavLink>
+    <NavLink exact to="/create" activeClassName="is-active">
+      Add
+    </NavLink>
+    <NavLink exact to="/help" activeClassName="is-active">
+      Help
+    </NavLink>
+  </header>
+)
+
+export default Header
